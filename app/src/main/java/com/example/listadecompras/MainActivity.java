1ed2d079c0c804c4a4c69765506f2aff59f9d3d5
Talitta <talittalima96@ggmail.com>
2020-09-09 16:21:48 -0300

    verson 1.1

--- app/src/main/java/com/example/listadecompras/MainActivity.java
+++ app/src/main/java/com/example/listadecompras/MainActivity.java
@@ -39,13 +39,13 @@ public class MainActivity extends AppCompatActivity {
         footer = (TextView)findViewById(R.id.footer);
 
         produtos = new ArrayList<>();
-        arrayAdapter = new ArrayAdapter(this, R.layout.simple_list_item_1, produtos);
+        arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, produtos);
         listView.setAdapter(arrayAdapter);
 
-        AdapterView.onItemLongClickListener itemClickListener = new AdapterView.onItemLongClickListener() {
+        AdapterView.OnItemLongClickListener itemClickListener = new AdapterView.OnItemLongClickListener() {
             public boolean onItemLongClick(AdapterView<?> listView, View view, int position, long id) {
                 final int localPosition = position;
-                new AlertDialog.Builder(MainActivity.this).setTitle("Remover Produto da Lista").setMessage("Voce realmente deseja excluir?").setIcon(R.drawable.ic_launcher_background).setPositiveButton(R.string.yes, new DialogInterface.OnClickListener() {
+                new AlertDialog.Builder(MainActivity.this).setTitle("Remover Produto da Lista").setMessage("Voce realmente deseja excluir?").setIcon(R.drawable.ic_launcher_foreground).setPositiveButton(R.string.yes, new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int whichButton) {
                         produtos.remove(localPosition);
                         arrayAdapter.notifyDataSetChanged();
@@ -56,7 +56,30 @@ public class MainActivity extends AppCompatActivity {
             }
         };
 
-        listView.setOnItemClickListener(itemClickListener);
+        listView.setOnItemLongClickListener(itemClickListener);
+       }
 
+       public void updateFooter() {
+           float total = 0;
+           float urgente = 0;
+           for (int i = 0; i < produtos.size(); i++) {
+               Produto produto = produtos.get(i);
+               if (produto.isUrgente()) {
+                   urgente += produto.getPreco();
+               }
+               total += produto.getPreco();
+           }
+           footer.setText("Total = " +total+ " : Urgennte = " +urgente);
+       }
+
+
+    public void addProduto( View view){
+        String item = String.valueOf(spinner.getSelectedItem());
+        boolean checked = checkBox.isChecked();
+        float preco = Float.parseFloat(editText.getText().toString());
+
+        Produto produto = new Produto(preco, item, checked);
+        produtos.add(produto);
+        arrayAdapter.notifyDataSetChanged();
     }
 }
